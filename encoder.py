afd86be990d5c023506fd3286af6fcc07b18ff8d
JeZzKaLyN24 <140087074+JeZzKaLyN24@users.noreply.github.com>
2023-10-25 20:04:23 -0400

    Added decoder definition

--- encoder.py
+++ encoder.py
@@ -1,3 +1,9 @@
+# Lab 6: Version Control 28
+# UF COP3502C Fall 2023
+# Encoder: Luke Xue
+# Decoder: Jessica Doten
+
+
 def encode(pwd):
     new_pwd = ""
     for x in pwd:
@@ -6,6 +12,13 @@ def encode(pwd):
         else:
             new_pwd += str(int(x)+3)
     return new_pwd
+
+def decode(pwd):
+    decoded_pwd = ""
+    for x in pwd:
+        decoded_pwd = decoded_pwd + str((int(x)-3)%10)
+    return decoded_pwd
+
 
 if __name__ == "__main__":
     while True:
@@ -19,5 +32,15 @@ if __name__ == "__main__":
         response = input("Please enter an option: ")
         if response == "1":
             pwd = input("Please enter your password to encode: ")
-            encode(pwd)
+            new_pwd = encode(pwd)
             print("Your password has been encoded and stored!")
+
+        if response == "2":
+            decoded_pwd = decode(pwd)
+            print(f'The encoded password is {new_pwd}, and the original password is {pwd}')
+
+        if response == "3":
+            break
+
+        else:
+            print("Invalid selection!\n")
